f51ff99cd76c007ca2425882aabb417cb2255204
GAndromidas <karlandromidas@gmail.com>
2026-01-26 13:33:09 +0200

    Support fish config sourcing from git clone directory

--- configs/user-fish-config/config.fish
+++ configs/user-fish-config/config.fish
@@ -1,7 +1,19 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# CachyOS Fish Configuration - User Config
+# This sources the system-wide cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
+# Try system path first (for installed CachyOS)
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
+# Fallback to home cachyinstaller folder (for development/git clone)
+else if test -f ~/cachyinstaller/configs/cachyos-fish-config/cachyos-config.fish
+    source ~/cachyinstaller/configs/cachyos-fish-config/cachyos-config.fish
+# Also check in .config if manually copied
+else if test -f ~/.config/fish/cachyos-config.fish
+    source ~/.config/fish/cachyos-config.fish
+end
+
+# User customizations below this line
+# Uncomment to override the greeting
 #function fish_greeting
-#    # smth smth
+#    # Custom greeting here
 #end
